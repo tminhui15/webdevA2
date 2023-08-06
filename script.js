20a14575ce9edfecc418bd12ca4d68df1969e238
tminhui15 <130944943+tminhui15@users.noreply.github.com>
2023-08-06 13:29:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+/* code for slideshow */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+/* code for scrolling animation */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+/* code for navigation bar fading out animation when scrolling */
+const navbar = document.querySelector('.navigation');
+
+function handleScroll() {
+
+  const scrollPosition = window.scrollY;
+  const fadeStart = 0;
+  const fadeEnd = 300;
+  const fadeRange = fadeEnd - fadeStart;
+  let opacity = 1 - (scrollPosition - fadeStart) / fadeRange;
+  opacity = Math.min(Math.max(opacity, 0), 1);
+  navbar.style.opacity = opacity;
+
+}
+window.addEventListener('scroll', handleScroll);
+
